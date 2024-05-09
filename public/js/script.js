2654c794cc03e6013b4e4f4d0fb2a02d13ed19d1
Ifti Dunne <ifti.dunne@gmail.com>
2024-05-08 20:11:09 -0600

    Enlarge images to center and minimize prior enlarged images

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,37 @@
 let img = document.getElementsByClassName('enlargeimg');
 
 function enlargeImage(img) {
-    img.style.transform =  'scale(2)';
+    img.classList.add('enlarged');
+    img.style.transform =  'scale(2) translate(-50%, -50%)';
     img.style.transition = 'transform 0.25s  ease';
     img.style.zIndex = '1';
+    img.style.position = 'fixed';
+    img.style.left = '50%';
+    img.style.top = '50%';
+    img.style.transformOrigin = 'top left';
+    img.style.width = '300px'; 
+    img.style.height = 'auto';
 }
 
 function resetImage(img) {
-    img.style.transform = 'scale(1)';
-    img.style.transform = 'transform 0.25s ease';
-    img.style.zIndex = '0';
+    img.classList.remove('enlarged');
+    img.style.transform = '';
+    img.style.transition = 'transform 0.25s  ease';
+    img.style.zIndex = '';
+    img.style.position = '';
+    img.style.left = '';
+    img.style.top = '';
+    img.style.transform = '';
+    img.style.transformOrigin = '';
+    img.style.width = ''; 
+    img.style.height = '';
 }
 
 const images = document.querySelectorAll('.enlargeimg');
 
 images.forEach(img => {
         img.addEventListener('click', function() {
-            if (img.style.transform === 'scale(2)') {
+            if (img.classList.contains('enlarged')) {
                 resetImage(img);
             } else {
                 enlargeImage(img);
