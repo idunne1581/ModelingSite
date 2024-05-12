9c8d92f351d49af49089ef12827b91af754aee4b
Ifti Dunne <ifti.dunne@gmail.com>
2024-05-11 19:57:49 -0600

    Add images

--- public/js/script.js
+++ public/js/script.js
@@ -1,41 +1,29 @@
-let img = document.getElementsByClassName('enlargeimg');
+let currentImage = null;
+const images = document.querySelectorAll('.enlargeimg');
 
 function enlargeImage(img) {
+    if (currentImage && currentImage !== img) {
+        resetImage(currentImage);
+    }
     img.classList.add('enlarged');
-    img.style.transform =  'scale(2) translate(-50%, -50%)';
-    img.style.transition = 'transform 0.25s  ease';
     img.style.zIndex = '1';
-    img.style.position = 'fixed';
-    img.style.left = '50%';
-    img.style.top = '50%';
-    img.style.transformOrigin = 'top left';
-    img.style.width = '300px'; 
-    img.style.height = 'auto';
+    currentImage = img;
 }
 
 function resetImage(img) {
     img.classList.remove('enlarged');
-    img.style.transform = '';
-    img.style.transition = 'transform 0.25s  ease';
     img.style.zIndex = '';
-    img.style.position = '';
-    img.style.left = '';
-    img.style.top = '';
-    img.style.transform = '';
-    img.style.transformOrigin = '';
-    img.style.width = ''; 
-    img.style.height = '';
+    currentImage = null;
 }
 
-const images = document.querySelectorAll('.enlargeimg');
-
 images.forEach(img => {
-        img.addEventListener('click', function() {
-            if (img.classList.contains('enlarged')) {
-                resetImage(img);
-            } else {
-                enlargeImage(img);
+    img.addEventListener('click', function() {
+        if (img.classList.contains('enlarged')) {
+            resetImage(img);
+        } else {
+            enlargeImage(img);
+        }
+    });
+});
 
-            }
-        })
-    })
+
