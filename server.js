e9af636c8f244d3119c918d54cc1385a056241ab
Ifti Dunne <ifti.dunne@gmail.com>
2024-05-20 20:30:56 -0600

    Correct path to views folder

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 require('dotenv').config({ path: './config/.env'});
@@ -6,21 +7,19 @@ require('dotenv').config({ path: './config/.env'});
 app.set('views', path.join(__dirname, 'views')); // Set the correct views directory
 app.set('view engine', 'ejs');
 
-app.set("view engine", "ejs");
-
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (request,response) => {
-        response.sendFile(__dirname + './public/index.html')
+        response.sendFile(path.join(__dirname, 'public', 'index.html'));
     });
 
 app.get('/portfolio', (request,response) => {
         response.render('portfolio')
     });    
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running, you better go catch it`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.Port || 3000}, you better go catch it`);
 });
